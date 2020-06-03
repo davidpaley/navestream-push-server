5b85e64683d1a41e82b9de01c8fb8dc054481f36
davidpaley <david.paleyy@gmail.com>
2020-06-03 19:53:09 -0300

    adding comment to send single push notif

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const io = require("socket.io")(http);
 
 io.on("connection", function(socket) {
   socket.on("new-message", function(data) {
-    console.log({ data });
+    console.log({ socketId: socket.id })
     io.emit("new-remote-message", data);
   });
 });
+
+// sending to individual socketid
+// socket.broadcast.to(socketid).emit('message', 'for your eyes only');
 
 http.listen(4000, function() {
   console.log("listening on *:4000");
